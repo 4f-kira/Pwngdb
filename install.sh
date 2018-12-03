84a7ab80e0af33e5b231202f9a8c2b28831a8e41
bash-c <aboultraman@gmail.com>
2018-12-03 11:23:06 +0800

    install

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ export LC_ALL=C
 # cd ~/
 git clone https://github.com/bash-c/Pwngdb.git --depth=1
 cd ./Pwngdb
+sudo apt-get install -y python-setuptools
 chmod +x ./pwndbg/setup.sh
 ./pwndbg/setup.sh
 cp ./.gdbinit ~/.gdbinit
